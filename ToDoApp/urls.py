47b6b462bc4df112f159960b4c596ccb77610c7c
Mena Ehab <127118141+commanderYT123@users.noreply.github.com>
2024-04-13 22:52:31 +0200

    set up adding, delte and delete task

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-urlpatterns = [
-    path("",views.index,name="index")
-]
 
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('delete-task/<int:pk>',views.deletePage,name='deletepage'),
+    path('delete/<int:pk>',views.delete,name='delete'),
+    path('edit/<int:pk>',views.editPage,name='edit'),
+]
--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
+
 def index(request):
+    tasks = Task.objects.all().order_by('-id')
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')  
+    else:
+        form = TaskForm()
     context = {
-        
+        'form': form,
+        'tasks':tasks,
     }
-    return render(request,"pages/index.html",context)
+    return render(request, "pages/index.html", context)
+
+def deletePage(request,pk):
+    task = Task.objects.filter(pk=pk).first()
+    context = {
+        'task':task
+    }
+    return render(request,"pages/delete.html",context)
+
+
+def delete(request,pk):
+    task = Task.objects.filter(pk=pk).first()
+    task.delete()
+    return redirect('index')
+
+
+def editPage(request,pk):
+    task = Task.objects.filter(pk=pk).first()
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    context = {
+        'task':task,
+        'form':form
+    }
+    return render(request,"pages/edit.html",context)
